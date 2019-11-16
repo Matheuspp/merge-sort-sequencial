14c3dabff4c3fd51dbf71fd0c00f1ba0fc937f88
matizonpp <matheusparaiso98@gmail.com>
2019-11-16 20:38:31 -0300

    modificado para gerar vetores de numeros aleatórios de tamanho n

--- src/merge_sort_s/Merge_sort_S.java
+++ src/merge_sort_s/Merge_sort_S.java
@@ -11,17 +11,21 @@ import java.util.*;
  * @author Matheus VR
  */
 public class Merge_sort_S {
+        
  
-	
-	static  int  vetor[]={100,20,15,30,5,75,40,10,25,111,1,43,89,0,2,3,21,53,46,101,112,134,156};
- 
+	//static  int  vetor[]={100,20,15,30,5,75,40,10,25,111,1,43,89,0,2,3,21,53,46,101,112,134,156,200,201,4,19,300,400,401,403,459,500};
+
 	public static void main(String args[])
 	{
+            
+                int[] vetor = new int[5000];
+                randomFill(vetor);
+            
 		long startTime = System.currentTimeMillis();
 		System.out.println("=================ANTES==================");
 		printVetor(vetor,0,vetor.length-1);
 		System.out.println("========================================");
-                mergeSort(0,vetor.length-1);
+                mergeSort(0,vetor.length-1, vetor);
 		System.out.println("=================DEPOIS=================");
  
 		
@@ -43,7 +47,7 @@ public class Merge_sort_S {
 	}
  
  
-	private static void merge(int start, int mid, int end) {
+	private static void merge(int start, int mid, int end, int vetor[]) {
 		
 		int[] tempArray=new int[vetor.length];
 		int tempArrayIndex=start;
@@ -87,18 +91,28 @@ public class Merge_sort_S {
         
         
         // Recursão :)
-	public static void mergeSort(int start,int end)
+	public static void mergeSort(int start,int end, int vetor[])
 	{
 		int mid=(start+end)/2;
 		if(start<end)
 		{
-                     mergeSort(start,mid);
-                     mergeSort(mid+1,end);
+                     mergeSort(start,mid, vetor);
+                     mergeSort(mid+1,end, vetor);
                     	
                         // direita e esquerda
-                     merge(start,mid,end);
+                     merge(start,mid,end, vetor);
 		}
  
 	}
+        
+        static void randomFill(int[] array)
+        {
+            Random rand = new Random();
+
+            for(int i = 0; i < array.length ; i++){
+                array[i] = rand.nextInt(1000);
+            }
+        }
+        
  
 }
